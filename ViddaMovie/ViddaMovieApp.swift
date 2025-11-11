608d7b4ce901000b5a593d93495cf60ee565fb3e
David Godfrey <kinqvidda@gmail.com>
2025-11-11 05:54:14 +0100

    Initial Commit

---
+++ ViddaMovie/ViddaMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViddaMovieApp.swift
+//  ViddaMovie
+//
+//  Created by Vidda on 11/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ViddaMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
