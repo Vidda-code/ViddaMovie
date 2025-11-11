5d822a08ac9b9bfbb6bdb5a429b7089be02ec4cd
David Godfrey <kinqvidda@gmail.com>
2025-11-11 06:28:31 +0100

    Masked all TabView Texts in a container

---
+++ ViddaMovie/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  ViddaMovie
+//
+//  Created by Vidda on 11/11/2025.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let homeString: String = "Home"
+    static let upcomingString: String = "Upcoming"
+    static let searchString: String = "Search"
+    static let downloadString: String = "Download"
+    
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let searchIconString = "magnifyingglass"
+    static let downloadIconString = "arrow.down.to.line.alt"
+}
